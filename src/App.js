0eb9031f354e3577da3e2b0fc252a8f148010a29
tayrotayro <rotolotaylor@gmail.com>
2020-02-13 21:16:19 -0600

    firebase installed but commented out

--- src/App.js
+++ src/App.js
@@ -1,52 +1,67 @@
 import React, { Component } from "react";
+import firebase from "firebase/app"; // import firebase package
+import "firebase/firestore"; // import firestore from firebase
 import "./App.scss";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAZXfyjF4BcjTgC0Uig_4-JpF3ZDkPwD_k",
-  authDomain: "lecture-app-tayro.firebaseapp.com",
-  databaseURL: "https://lecture-app-tayro.firebaseio.com",
-  projectId: "lecture-app-tayro",
-  storageBucket: "lecture-app-tayro.appspot.com",
-  messagingSenderId: "17246310095",
-  appId: "1:17246310095:web:47acefcd35b6a1955d31ba"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAZXfyjF4BcjTgC0Uig_4-JpF3ZDkPwD_k",
+//   authDomain: "lecture-app-tayro.firebaseapp.com",
+//   databaseURL: "https://lecture-app-tayro.firebaseio.com",
+//   projectId: "lecture-app-tayro",
+//   storageBucket: "lecture-app-tayro.appspot.com",
+//   messagingSenderId: "17246310095",
+//   appId: "1:17246310095:web:47acefcd35b6a1955d31ba"
+// }; // config key (from project settings)
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig); // initilize firebase app
+// const db = firebaseApp.firestore(); // initializes the firestore database object
+// const counterRef = db.collection("app-data").doc("VUJ9FOKlkxDxLlwfphtL"); // create a reference to the document which stores the counter
+// const increment = firebase.firestore.FieldValue.increment(1); // increment function (a firestore feature - refer to documentation for more details)
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      updating: false
+      counter: 0
     };
   }
 
-  increase = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-      updating: true
-    });
-    this.reset();
-  };
+  // READ FIRESTORE DATABASE
+  // componentDidMount() {
+  //   // counterRef
+  //   //   .get()
+  //   //   .then(doc => {
+  //   counterRef.onSnapshot(doc => {
+  //     if (doc.exists) {
+  //       this.setState({
+  //         counter: doc.data().counter
+  //       });
+  //     } else {
+  //       console.log("No document");
+  //     }
+  //   });
+  //   // .catch(err => {
+  //   //   console.log("Error: ", err);
+  //   // });
+  // }
 
-  reset = () => {
-    setTimeout(() => {
-      this.setState({ updating: false });
-    }, 1000);
+  // UPDATE FIRESTORE
+  increase = () => {
+    //counterRef.update({ counter: increment });
+    this.setState({
+      counter: this.state.counter + 1
+    });
   };
 
   render() {
-    const { counter, updating } = this.state;
+    const { counter } = this.state;
 
     return (
       <div className="app">
         <div className="counter">{counter}</div>
-        {updating && (
-          <div className="counter-button disabled">UPDATING... 😁👍🏼</div>
-        )}
-        {!updating && (
-          <div className="counter-button" onClick={this.increase}>
-            CLICK ME!
-          </div>
-        )}
+        <div className="counter-button" onClick={this.increase}>
+          CLICK ME!
+        </div>
       </div>
     );
   }
